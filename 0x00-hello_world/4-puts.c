ad3bec28e83a6ed243f07700e8dfb5cc9f360caf
root <Hassandanfillo@gmail.com>
2022-12-08 05:54:55 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 
+/**
+ *main= entry point
+ *
+ * return= always zero
+ **/
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
