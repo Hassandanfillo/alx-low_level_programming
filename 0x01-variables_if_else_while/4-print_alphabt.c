7036a724c4c80c1f41286119f0f849a709414dd2
root <Hassandanfillo@gmail.com>
2022-12-10 14:33:11 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int ch;
 
-	for (ch = 'a'; ch <= 'z' && ch != 'q', 'e'; ch++)
+	for (ch = 'a'; ch <= 'z' && ch != 'q'|| 'e'; ch++)
 		putchar(ch);
 	putchar('\n');
 	return (0);
