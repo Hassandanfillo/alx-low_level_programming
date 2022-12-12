880ea53818a9339621a3784864c41bb94f3141fa
root <Hassandanfillo@gmail.com>
2022-12-12 06:03:58 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ *
+ * Return: Always 0 (success)
+**/
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	i = 0;
+	while (i < 1000)
+	{
+		j = i / 100;
+		k = (i / 10) % 10;
+		l = i % 100;
+		if (j < k && k < l)
+		{
+			putchar(l + '0');
+			putchar(k + '0');
+			putchar(j + '0');
+			if (i < 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		i++;
+	}
+	return (0);
+}
+
+
