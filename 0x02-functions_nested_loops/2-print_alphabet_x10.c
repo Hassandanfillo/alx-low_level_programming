970e8bd3434b39f411b2cc779bfaa12e6d12b99e
root <Hassandanfillo@gmail.com>
2022-12-14 00:53:41 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+**/
+
+void print_alphabet_x10(void)
+{
+	int lines;
+	int cha;
+
+	while (lines <= 10)
+	{
+		for (cha = 'a'; cha <= 'z'; cha++)
+		{
+			putchar(cha);
+			putchar('\n');
+		}
+		lines++;
+	}
+}
